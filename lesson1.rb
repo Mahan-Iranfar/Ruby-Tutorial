547c391f1af504a512a3ea40bc7e38d3873786d3
Mahan-Iranfar <mahaniranfar1@gmail.com>
2025-05-11 00:20:30 +0330

    Add files via upload

---
+++ lesson1.rb
@@ -0,0 +1,33 @@
+puts "hello, Ruby!"
+
+
+
+# case-sensetive
+# Puts "hello"
+
+puts       "hello, Ruby!"
+
+puts "ruby", "is", "the", "best"
+
+
+
+# BEGIN, END
+BEGIN {
+  puts "BEGIN statement"
+}
+
+END {
+  puts "End statement"
+}
+
+
+
+# this is a comment
+
+=begin
+this
+is
+a
+multi-line
+comment
+=end
